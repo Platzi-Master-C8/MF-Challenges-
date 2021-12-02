46b77f0536170c625074c113b54854c90ba54eec
Daniel Sanchez <danielsanchez1502@gmail.com>
2021-12-02 13:57:43 -0300

    feat: system point section with styles

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import '../styles/global.scss';
+import System from "./sections/System";
+
+const App = () => <System />
+
+export default App;
---
+++ src/components/systempoint/System.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Component } from 'react';
+
+
+
+class System extends Component {
+        constructor() {
+          super();
+          this.state = { data: [] };
+        }
+      
+        async componentDidMount() {
+          const response = await fetch(`http://localhost:3000/data`);
+          const json = await response.json();
+          this.setState({ data: json });
+        }
+    
+
+    render() {
+        return (
+            <div className="container">
+                <div className="first-circle">
+                        <p>{this.state.data.points}</p>
+                </div>
+                <p>Points Obtained</p>
+                <div className="second-circle">
+                    <p>101</p>
+                </div>
+                <p>Next Rank</p>
+            </div>
+        )
+    }
+    
+}
+
+export default System;
