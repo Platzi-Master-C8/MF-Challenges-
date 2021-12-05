6eb8bef0a7ae78575d576eae93aab9f94e86835e
carlosmen <carlosamenesesc@gmail.com>
2021-12-05 14:52:45 -0600

    Resolviendo conflcvtos

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,34 +1,38 @@
-import React from 'react'
-import Avatar from '../../icons/Avatar.png'
-import Dashboard from '../../icons/Dashboard.svg'
-import Forum from '../../icons/forum.svg'
-import Challanges from '../../icons/challenges.svg'
-import Tasks from '../../icons/Tasks.svg'
-import Profile from '../../icons/profile.svg'
+import React from "react";
+import Avatar from '../../icons/Avatar.png';
+import Dashboard from '../../icons/Dashboard.svg';
+import Forum from '../../icons/forum.svg';
+import Challanges from '../../icons/challenges.svg';
+import Tasks from '../../icons/Tasks.svg';
+import Profile from '../../icons/profile.svg';
+import data from '../../../data.json';
+
+const { name } = data;
+
 
 const NavBar = () => {
   return (
-    <aside className='navBar'>
-      <div className='navBar-user'>
-        <a href='#'>Carlos Meneses</a>
-        <img src={Avatar} alt='' />
+    <aside className="navBar">
+      <div className="navBar-user">
+        <a href="#" >{name}</a>
+        <img src={ Avatar }alt=""/>
       </div>
       <div className='navBar-menu'>
         <ul>
           <li>
-            <a href='#'><img className='img-icon' src={Dashboard} alt='' /><p className='navBar-menu__text'>Dashboard</p></a>
+            <a href="#"><img id="img-icon" src={Dashboard} alt="" /><p className="navBar-menu__text">Estadisticas</p></a>
           </li>
           <li>
-            <a href='#'><img className='img-icon' src={Forum} alt='' /><p className='navBar-menu__text'>Forum</p></a>
+            <a href="#"><img id="img-icon" src={Forum} alt="" /><p className="navBar-menu__text">Foro</p></a>
           </li>
           <li>
-            <a href='#'><img className='img-icon' src={Challanges} alt='' /><p className='navBar-menu__text'>Challenges</p></a>
+            <a href="#"><img id="img-icon" src={Challanges} alt="" /><p className="navBar-menu__text">Desafios</p></a>
           </li>
           <li>
-            <a href='#'><img className='img-icon' src={Tasks} alt='' /><p className='navBar-menu__text'>Tasks</p></a>
+            <a href="#"><img id="img-icon" src={Tasks} alt="" /><p className="navBar-menu__text">Tareas</p></a>
           </li>
           <li>
-            <a href='#'><img className='img-icon' src={Profile} alt='' /><p className='navBar-menu__text'>Profile</p></a>
+            <a href="#"><img id="img-icon" src={Profile} alt="" /><p className="navBar-menu__text">Perfil</p></a>
           </li>
         </ul>
       </div>
