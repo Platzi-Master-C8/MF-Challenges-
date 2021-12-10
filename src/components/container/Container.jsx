11f6871ba0bfa8f21a17128b6aa96be8190c134c
carlosmen <carlosamenesesc@gmail.com>
2021-12-10 07:47:30 -0600

    Implement of the react-router-dom

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
-/* import System from '../systempoint/System'; */
+import System from '../systempoint/System';
 import Header from '../header/Header';
 import {Link} from 'react-router-dom'
-/* import Navbar from '../navbar/Navbar';
+import Navbar from '../navbar/Navbar';
 import StreakAndRank from '../StreakAndRank/StreakAndRank';
 import UserGraph from '../user-graph/user-graph';
 import Achievements from '../achievements/Achievements';
-import useGetData from '../../hooks/useGetData'; */
+import useGetData from '../../hooks/useGetData';
 const API = 'http://localhost:3500/data';
 const Container = () => {
-  /* const Data = useGetData(API); */
+  const Data = useGetData(API);
   return (
     <div className="grid-container">
       <Header />
-      <Link to='/editor'>
-        EDITOR
-      </Link>
-      {/* <Navbar /> */}
-      {/* <StreakAndRank ranks={Data.ranks} challenges={Data.challenges} />
+      <Navbar />
+      <StreakAndRank ranks={Data.ranks} challenges={Data.challenges} />
       <System data={Data} ranks={Data.ranks} />
       <Achievements goals={Data.achievements} />
-      <UserGraph activity={Data.activity} /> */}
+      <UserGraph activity={Data.activity} />
       <div className="item-footer"></div>
     </div>
   );
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Avatar from '../../icons/Avatar.png'
+import Dashboard from '../../icons/Dashboard.svg'
+import Forum from '../../icons/forum.svg'
+import Challanges from '../../icons/challenges.svg'
+import Tasks from '../../icons/Tasks.svg'
+import Profile from '../../icons/profile.svg'
+import datos from '../../../data.json'
+
+import { Link } from 'react-router-dom'
+
+
+const { data } = datos;
+
+
+
+const NavBar = () => {
+  return (
+    <div className="item-navbar">
+      <aside className='navBar'>
+      <div className='navBar-user'>
+        <a href='#'> { data.name } </a>
+        <img src={Avatar} alt='' />
+      </div>
+      <div className='navBar-menu'>
+        <ul>
+          <li>
+            <a href='#'><img id='img-icon' src={Dashboard} alt='' /><p className='navBar-menu__text'>Estadisticas</p></a>
+          </li>
+          <li>
+            <a href='#'><img id='img-icon' src={Forum} alt='' /><p className='navBar-menu__text'>Foro</p></a>
+          </li>
+            <li>
+              <Link to='/editor'>
+                <img id='img-icon' src={Challanges} alt='' /><p className='navBar-menu__text'>Desafios</p>
+              </Link>
+          </li>
+          <li>
+            <a href='#'><img id='img-icon' src={Tasks} alt='' /><p className='navBar-menu__text'>Tareas</p></a>
+          </li>
+          <li>
+            <a href='#'><img id='img-icon' src={Profile} alt='' /><p className='navBar-menu__text'>Perfil</p></a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+    </div>
+  )
+}
+
+export default NavBar
